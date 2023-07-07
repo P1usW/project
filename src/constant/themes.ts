00d4566aedd288bd06a427b4280e51368a27d055
Ivan Shavrin <ivshavrin@gmail.com>
2023-07-07 12:32:18 +0700

    add theme utils

--- src/constant/themes.ts
+++ src/constant/themes.ts
@@ -1,6 +1,12 @@
 const themes = {
   dark: 'dark',
   light: 'light',
-}
+} as const
+
+type ThemesType = typeof themes[keyof typeof themes]
 
 export default themes
+
+export type {
+  ThemesType
+}
--- src/utils/getTheme.ts
+++ src/utils/getTheme.ts
@@ -1,13 +1,15 @@
-import themes from "@src/constant/themes";
+import themes, { ThemesType } from "@src/constant/themes";
 
 /**
  * Функция, которая ищет значение темы у пользователя с локального хранилища,
  * если такой не существует, то устанавливает тему из операционной системы.
  * Если тему не удалось определить, ставим светлую тему как основную.
  */
-const getTheme = () => {
+const getTheme = (): ThemesType => {
   // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
   const theme = `${window?.localStorage?.getItem('theme')}`;
+  
+  // @ts-ignore
   if (Object.values(themes).includes(theme)) return theme;
 
   const userMedia = matchMedia('(prefers-color-scheme: dark)');
